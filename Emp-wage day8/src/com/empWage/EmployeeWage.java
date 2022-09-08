8394e4144100323253016cb60293aa79ae450eb3
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-09-08 19:59:35 +0530

    Calculate daily employee wage

--- Emp-wage day8/src/com/empWage/EmployeeWage.java
+++ Emp-wage day8/src/com/empWage/EmployeeWage.java
@@ -7,11 +7,19 @@ public class EmployeeWage {
 
 		final int present = 1;
 		final int absent = 2;
+		int empHrs = 0;
+		int empWage = 0;
+		int empRatePerHr = 20;
 		int empCheck = (int) (Math.random() * 2) + 1;
 
 		if (empCheck == present) {
 			System.out.println("Employee is present");
-		} else
+			empHrs = 8;
+		} else {
 			System.out.println("Employee is absent");
+			empHrs = 0;
+		}
+		empWage = empHrs * empRatePerHr;
+		System.out.println("employee daily wage : " + empWage);
 	}
 }
