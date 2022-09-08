5640906043ff478090da3350c7411428cfc9959c
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-09-08 20:36:35 +0530

    Refactor to write class method

--- Emp-wage day8/src/com/empWage/EmployeeWage.java
+++ Emp-wage day8/src/com/empWage/EmployeeWage.java
@@ -1,21 +1,18 @@
 package com.empWage;
 
 public class EmployeeWage {
-	public static void main(String[] args) {
+	static final int isFullTime = 1;
+	static final int isPartTime = 2;
+	static final int isAbsent = 3;
+	static final int maxWorkingHrs = 100;
+	static final int maxWorkingDays = 20;
+	static final int empRatePerHr = 20;
 
-		System.out.println("Welcome to employee Wage");
-
-		final int isFullTime = 1;
-		final int isPartTime = 2;
-		final int isAbsent = 3;
-		final int maxWorkingHrs = 100;
-		final int maxWorkingDays = 20;
-
+	public static void computeWage() {
 		int empHrs = 0;
 		int empWage = 0;
-		int empRatePerHr = 20;
-		int totalEmpHrs = 0;
 		int days = 0;
+		int totalEmpHrs = 0;
 
 		while (days < maxWorkingDays && totalEmpHrs <= maxWorkingHrs) {
 			int empCheck = (int) (Math.random() * 3) + 1;
@@ -39,5 +36,9 @@ public class EmployeeWage {
 		System.out.println("Total employee hours : " + totalEmpHrs);
 		empWage = totalEmpHrs * empRatePerHr;
 		System.out.println("employee Monthly wage : " + empWage);
+	}
+
+	public static void main(String[] args) {
+		computeWage();
 	}
 }
