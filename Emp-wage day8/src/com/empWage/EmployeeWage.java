8bf97d6d7514c2bb2065d8e3adaf6f6b89e0ed20
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-09-08 19:53:50 +0530

    checking employee attendance

--- Emp-wage day8/src/com/empWage/EmployeeWage.java
+++ Emp-wage day8/src/com/empWage/EmployeeWage.java
@@ -2,7 +2,16 @@ package com.empWage;
 
 public class EmployeeWage {
 	public static void main(String[] args) {
+
 		System.out.println("Welcome to employee Wage");
 
+		final int present = 1;
+		final int absent = 2;
+		int empCheck = (int) (Math.random() * 2) + 1;
+
+		if (empCheck == present) {
+			System.out.println("Employee is present");
+		} else
+			System.out.println("Employee is absent");
 	}
 }
