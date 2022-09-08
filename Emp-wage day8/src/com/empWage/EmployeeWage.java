cd61060553c925d4206e6d7a9e9ca281f3a9167f
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-09-08 20:03:46 +0530

    Adding part time & wage

--- Emp-wage day8/src/com/empWage/EmployeeWage.java
+++ Emp-wage day8/src/com/empWage/EmployeeWage.java
@@ -5,16 +5,21 @@ public class EmployeeWage {
 
 		System.out.println("Welcome to employee Wage");
 
-		final int present = 1;
-		final int absent = 2;
+		final int isFullTime = 1;
+		final int isPartTime = 2;
+		final int isAbsent = 3;
+
 		int empHrs = 0;
 		int empWage = 0;
 		int empRatePerHr = 20;
-		int empCheck = (int) (Math.random() * 2) + 1;
+		int empCheck = (int) (Math.random() * 3) + 1;
 
-		if (empCheck == present) {
-			System.out.println("Employee is present");
+		if (empCheck == isFullTime) {
+			System.out.println("Employee is full time");
 			empHrs = 8;
+		} else if (empCheck == isPartTime) {
+			System.out.println("Employee is part time");
+			empHrs = 4;
 		} else {
 			System.out.println("Employee is absent");
 			empHrs = 0;
