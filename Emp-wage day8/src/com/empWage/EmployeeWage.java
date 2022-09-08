9e0304be685bf2b4f42d6530020864b54bbfd11d
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-09-08 20:16:55 +0530

    added monthly wage

--- Emp-wage day8/src/com/empWage/EmployeeWage.java
+++ Emp-wage day8/src/com/empWage/EmployeeWage.java
@@ -9,26 +9,32 @@ public class EmployeeWage {
 		final int isPartTime = 2;
 		final int isAbsent = 3;
 
+		int totalWorkingDays = 20;
 		int empHrs = 0;
 		int empWage = 0;
 		int empRatePerHr = 20;
-		int empCheck = (int) (Math.random() * 3) + 1;
+		int totalEmpHrs = 0;
 
-		switch (empCheck) {
-		case isFullTime:
-			System.out.println("Employee is full time");
-			empHrs = 8;
-			break;
-		case isPartTime:
-			System.out.println("Employee is part time");
-			empHrs = 4;
-			break;
-		case isAbsent:
-			System.out.println("Employee is absent");
-			empHrs = 0;
-			break;
+		for (int days = 0; days < totalWorkingDays; days++) {
+			int empCheck = (int) (Math.random() * 3) + 1;
+			switch (empCheck) {
+			case isFullTime:
+				System.out.println("Employee is full time");
+				empHrs = 8;
+				break;
+			case isPartTime:
+				System.out.println("Employee is part time");
+				empHrs = 4;
+				break;
+			case isAbsent:
+				System.out.println("Employee is absent");
+				empHrs = 0;
+				break;
+			}
+			totalEmpHrs += empHrs;
 		}
-		empWage = empHrs * empRatePerHr;
-		System.out.println("employee daily wage : " + empWage);
+		System.out.println("Total employee hours : " + totalEmpHrs);
+		empWage = totalEmpHrs * empRatePerHr;
+		System.out.println("employee Monthly wage : " + empWage);
 	}
 }
